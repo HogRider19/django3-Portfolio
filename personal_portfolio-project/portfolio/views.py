8ddb5a2434060944fa185c0fb8e2c56153c06a46
HorRider <danilzzzg19@gmail.com>
2022-06-13 01:18:02 +0300

    End of the first day of work!

--- personal_portfolio-project/portfolio/views.py
+++ personal_portfolio-project/portfolio/views.py
@@ -1,3 +1,8 @@
 from django.shortcuts import render
+from .models import Project
 
-# Create your views here.
+
+def home(request):
+    #Импорт всех данных из базы данных
+    progects = Project.objects.all()
+    return render(request, 'portfolio/home_page.html', {'projects' : progects})
